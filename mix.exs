351f43e8a416f60ff8711506f1ac9a2a9dfd12ba
kagure-nyakio <kagurenyakio@gmail.com>
2025-03-08 19:25:49 +0300

    Update context task to create factories in place of fixtures

--- mix.exs
+++ mix.exs
@@ -142,6 +142,7 @@ defmodule BemedaPersonal.MixProject do
         "esbuild bemeda_personal --minify",
         "phx.digest"
       ],
+      "phx.gen.context": "phx.gen.context_factories",
       check_code: [
         "deps.unlock --check-unused",
         # TODO: Remove the flag once the hackney issue is fixed
