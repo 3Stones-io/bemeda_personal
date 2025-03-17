1290929e6dd15e5d0053ea768f21a82567ad3c5b
kagure-nyakio <kagurenyakio@gmail.com>
2025-03-17 12:17:49 +0300

    Fix dialyzer issues

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,4 @@
 [
-  "lib/credo_checks/readability/import_order.ex"
+  {"lib/credo_checks/readability/import_order.ex", :callback_info_missing},
+  {"lib/credo_checks/readability/import_order.ex", :unknown_function}
 ]
