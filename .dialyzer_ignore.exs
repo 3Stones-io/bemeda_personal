38d5889baf1624ea4bedae280b9362c4d585fd82
Almir Sarajčić <almir.sarajcic@icloud.com>
2025-03-19 17:01:02 +0300

    Fix kwa kweli

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,4 @@
 [
-  {"lib/credo_checks/readability/import_order.ex", :callback_info_missing},
-  {"lib/credo_checks/readability/import_order.ex", :unknown_function}
+  {"test/support/credo_checks/readability/import_order.ex", :callback_info_missing},
+  {"test/support/credo_checks/readability/import_order.ex", :unknown_function}
 ]
