400f8bcf9032b67f153cfe65535773d6dfb1bfbf
kagure-nyakio <kagurenyakio@gmail.com>
2025-04-22 05:26:38 +0300

    fix migration issue

--- priv/repo/migrations/20250416153424_rename_chat_mux_data_to_media_data.exs
+++ priv/repo/migrations/20250416153424_rename_chat_mux_data_to_media_data.exs
@@ -1,10 +1,17 @@
 defmodule BemedaPersonal.Repo.Migrations.RenameChatMuxDataToMediaData do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:messages) do
       remove :mux_data
       add :media_data, :map
+    end
+  end
+
+  def down do
+    alter table(:messages) do
+      remove :media_data
+      add :mux_data, :map
     end
   end
 end
