cc0f9cee0779c62cb41bef55c8b65b8eb818d6dd
Almir Sarajčić <almir.sarajcic@icloud.com>
2025-05-01 18:09:58 +0200

    Fix migrations

--- priv/repo/migrations/20250423142258_remove_mux_data_from_job_applications.exs
+++ priv/repo/migrations/20250423142258_remove_mux_data_from_job_applications.exs
@@ -4,13 +4,12 @@ defmodule BemedaPersonal.Repo.Migrations.RemoveMuxDataFromJobApplications do
   def up do
     execute """
     INSERT INTO media_assets (
-      id, file_name, type, upload_id, job_application_id, status, inserted_at, updated_at
+      id, file_name, type, job_application_id, status, inserted_at, updated_at
     )
     SELECT
       gen_random_uuid(),
       mux_data->>'file_name',
       mux_data->>'type',
-      (mux_data->>'upload_id')::uuid,
       id,
       'uploaded',
       NOW(),
@@ -35,8 +34,7 @@ defmodule BemedaPersonal.Repo.Migrations.RemoveMuxDataFromJobApplications do
     UPDATE job_applications
     SET mux_data = jsonb_build_object(
       'file_name', ma.file_name,
-      'type', ma.type,
-      'upload_id', ma.upload_id
+      'type', ma.type
     )
     FROM media_assets ma
     WHERE ma.job_application_id = job_applications.id
--- priv/repo/migrations/20250423143046_remove_mux_data_from_messages.exs
+++ priv/repo/migrations/20250423143046_remove_mux_data_from_messages.exs
@@ -4,13 +4,12 @@ defmodule BemedaPersonal.Repo.Migrations.RemoveMuxDataFromMessages do
   def up do
     execute """
     INSERT INTO media_assets (
-      id, file_name, type, upload_id, message_id, status, inserted_at, updated_at
+      id, file_name, type, message_id, status, inserted_at, updated_at
     )
     SELECT
       gen_random_uuid(),
       mux_data->>'file_name',
       mux_data->>'type',
-      (mux_data->>'upload_id')::uuid,
       id,
       'uploaded',
       NOW(),
@@ -35,8 +34,7 @@ defmodule BemedaPersonal.Repo.Migrations.RemoveMuxDataFromMessages do
     UPDATE messages
     SET mux_data = jsonb_build_object(
       'file_name', ma.file_name,
-      'type', ma.type,
-      'upload_id', ma.upload_id
+      'type', ma.type
     )
     FROM media_assets ma
     WHERE ma.message_id = messages.id
