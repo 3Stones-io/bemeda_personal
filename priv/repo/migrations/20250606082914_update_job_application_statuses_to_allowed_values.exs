eba5026be2b4c2347750ef29e86067ebed3fc9d9
kagure-nyakio <kagurenyakio@gmail.com>
2025-06-06 12:22:50 +0300

    Update migration for existing applications

---
+++ priv/repo/migrations/20250606082914_update_job_application_statuses_to_allowed_values.exs
@@ -0,0 +1,16 @@
+defmodule BemedaPersonal.Repo.Migrations.UpdateJobApplicationStatusesToAllowedValues do
+  use Ecto.Migration
+
+  def up do
+    execute """
+    UPDATE job_applications
+    SET state = CASE
+      WHEN state = 'rejected' THEN 'withdrawn'
+      WHEN state = 'offer_declined' THEN 'withdrawn'
+      WHEN state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn') THEN 'applied'
+      ELSE state
+    END
+    WHERE state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
+    """
+  end
+end
