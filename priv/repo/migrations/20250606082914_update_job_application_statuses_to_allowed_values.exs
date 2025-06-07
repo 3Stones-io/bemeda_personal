6070547c6fe0cd12d5674cc307205fa9131175ce
kagure-nyakio <kagurenyakio@gmail.com>
2025-06-07 07:10:36 +0300

    Fix migration bug

--- priv/repo/migrations/20250606082914_update_job_application_statuses_to_allowed_values.exs
+++ priv/repo/migrations/20250606082914_update_job_application_statuses_to_allowed_values.exs
@@ -13,4 +13,14 @@ defmodule BemedaPersonal.Repo.Migrations.UpdateJobApplicationStatusesToAllowedVa
     WHERE state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
     """
   end
+
+  def down do
+    execute """
+    UPDATE job_applications
+    SET state = CASE
+      WHEN state = 'withdrawn' THEN 'rejected'
+      ELSE state
+    END
+    """
+  end
 end
