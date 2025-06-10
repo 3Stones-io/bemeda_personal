a8da245e8bb8bb07e752ab6d685f2fc15325c9d8
kagure-nyakio <kagurenyakio@gmail.com>
2025-06-10 07:28:29 +0300

    Update migration

--- priv/repo/migrations/20250607075512_update_job_application_state_transitions_to_allowed_values.exs
+++ priv/repo/migrations/20250607075512_update_job_application_state_transitions_to_allowed_values.exs
@@ -3,6 +3,36 @@ defmodule BemedaPersonal.Repo.Migrations.UpdateJobApplicationStateTransitionsToA
 
   def up do
     execute """
+    CREATE TABLE job_application_state_transitions_backup AS
+    SELECT * FROM job_application_state_transitions
+    WHERE from_state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
+       OR to_state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
+    """
+
+    execute """
+    UPDATE job_application_state_transitions
+    SET from_state = 'applied',
+        to_state = CASE
+          WHEN to_state IN ('offer_extended', 'withdrawn') THEN to_state
+          ELSE 'offer_extended'
+        END
+    WHERE from_state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
+    """
+
+    execute """
+    UPDATE job_application_state_transitions
+    SET to_state = CASE
+      WHEN from_state = 'applied' AND to_state NOT IN ('offer_extended', 'withdrawn') THEN 'offer_extended'
+      WHEN from_state = 'offer_extended' AND to_state NOT IN ('offer_accepted', 'withdrawn') THEN 'offer_accepted'
+      WHEN from_state = 'withdrawn' AND to_state NOT IN ('applied', 'offer_accepted') THEN 'applied'
+      WHEN from_state = 'offer_accepted' THEN to_state
+      ELSE to_state
+    END
+    WHERE to_state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
+      AND from_state IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
+    """
+
+    execute """
     DELETE FROM job_application_state_transitions
     WHERE from_state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
        OR to_state NOT IN ('applied', 'offer_extended', 'offer_accepted', 'withdrawn')
@@ -10,5 +40,13 @@ defmodule BemedaPersonal.Repo.Migrations.UpdateJobApplicationStateTransitionsToA
   end
 
   def down do
+    execute """
+    INSERT INTO job_application_state_transitions
+    SELECT * FROM job_application_state_transitions_backup
+    """
+
+    execute """
+    DROP TABLE job_application_state_transitions_backup
+    """
   end
 end
